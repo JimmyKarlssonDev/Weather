8f7dfd0ed60e1b5ee10341009b68f27e8161ad53
Jimmy Karlsson <jimmy.ch.karlsson.dev@gmail.com>
2019-03-11 12:32:15 +0100

    Add weather icons

--- src/Repository.js
+++ src/Repository.js
@@ -1,4 +1,5 @@
 
+
 const WeatherData = {
     "list": [
       {
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import repository from "../Repository";
 function getDayOfWeek(date) {  
   console.log(date);
   var dayOfWeek = new Date(date).getDay();    
-  console.log(dayOfWeek);
   return isNaN(dayOfWeek) ? null : ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][dayOfWeek];
 }
 
@@ -13,16 +12,17 @@ function convertKelvinToCelsius(kelvin) {
 	if (kelvin < (0)) {
 		return 'below absolute zero (0 K)';
 	} else {
-		return Math.round((kelvin-273.15));
+		return Math.round((kelvin-273.15)); 
 	}
 }
 
-
-const DayWeather = props => ( 
+const DayWeather = props => (                         
   <div className="weather-day">
   <div className="week-day" >  
     <div className="day">{getDayOfWeek(props.date)}</div>
-    <div className="weatherImg">IMG</div>
+    <div className="weatherImg">
+      <img src={`https://openweathermap.org/img/w/${props.weather[0].icon}.png`}/>
+    </div>
     <div className="temps">   
       <div className="highTemp">{`${convertKelvinToCelsius(props.temps.temp_max)} °C`}</div>
       <div className="lowTemp">{`${convertKelvinToCelsius(props.temps.temp_min)} °C`}</div>
